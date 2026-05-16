fc4368bbbf5499486c830cfe1531287679126503
Moon <mooninfinityalways@gmail.com>
2026-05-16 20:35:10 +0300

    I

--- HW/HW2/I.cpp
+++ HW/HW2/I.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
 #include <cstdint>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+#define LD long double
+
+struct TL {
+    LD a, b, p;
+};
 
 int main() {
-    int64_t T, D, N;
+    LD T, D;
+    int64_t N;
     std::cin >> T >> D >> N;
-    int64_t a[N], b[N], p[N];
-    for (int64_t i = 0; i < N; ++i) std::cin >> a[i];
-    for (int64_t i = 0; i < N; ++i) std::cin >> b[i];
-    for (int64_t i = 0; i < N; ++i) std::cin >> p[i];
-    int64_t left = 0, right = a[N - 1];
-    auto f = [&](int64_t speed) {
+    std::vector<TL> tl(N);
+    for (int64_t i = 0; i < N; ++i) std::cin >> tl[i].a >> tl[i].b >> tl[i].p;
+    std::sort(tl.begin(), tl.end(), [](TL a, TL b){return a.p < b.p;});
+    LD left = 0, right = 1e18;
+    auto f = [&](LD speed) {
+        LD time = 0;
+        LD past = 0;
+        for (int64_t i = 0; i < N && tl[i].p <= D; ++i) {
+            time += ((tl[i].p - past)/speed);
+            LD phase = fmod(time, tl[i].a + tl[i].b);
+            if (phase < tl[i].a) {
+                time += tl[i].a - phase;
+            }
+            past = tl[i].p;
+        }
+        time += (D - past) / speed;
+        return time;
     };
-    while (right - left > 1) {
-        int64_t mid = (right + left) / 2;
-        if (f(mid)) right = mid;
+    for (int64_t i = 0; i < 1000; ++i) {
+        LD mid = (right + left) / 2;
+        if (f(mid) <= T) right = mid;
         else left = mid;
     }
-    std::cout << left;
+    std::cout << std::setprecision(10) << std::fixed;
+    if (right == 1e18) std::cout << -1.0;
+    else std::cout << right;
 }
