548540cbf36aecce092f78065cd172f668834493
Moon <mooninfnityalways@gmail.com>
2026-06-01 20:40:27 +0300

    dekart

---
+++ sem/Dekart_tree.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <random>
+#include <cstdint>
+
+std::mt19937_64 rd(time(nullptr));
+
+struct Node {
+    int64_t value_ = 0;
+    uint64_t p = rd();
+    Node* left = nullptr;
+    Node* right = nullptr;
+    Node(int64_t value) : value_(value) {}
+    ~Node() {
+        delete left;
+        delete right;
+    }
+};
+
+class Treap {
+private:
+    Node* root = nullptr;
+    Node* merge(Node* less, Node*more) {
+        if (!less) return more;
+        if (!more) return less;
+        if (less->p > more->p) {
+            less->right = merge(less->right, more);
+            return less;
+        }
+        more->left = merge(less, more->left);
+        return more;
+    }
+
+    std::pair<Node*, Node*> split(Node* tree, int64_t key) {
+        if (!tree) return {nullptr, nullptr};
+        if (tree->value_ > key) {
+            auto[less, more] = split(tree->left, key);
+            tree->left = more;
+            return {less, tree};
+        }
+        auto[less, more] = split(tree->right, key);
+        tree->right = less;
+        return {tree, more};
+    }
+
+    void print(Node * tree) {
+        if (!tree) return;
+        print(tree->left);
+        std::cout << tree->value_ << " ";
+        print(tree->right);
+    }
+
+    int64_t hi(Node * tree) {
+        if (!tree) return 0;
+        return std::max(hi(tree->left), hi(tree->right)) + 1;
+    }
+
+public:
+
+    void insert(int64_t value) {
+        auto[less, more] = split(root, value);
+        root = merge(less, new Node(value));
+        root = merge(root, more);
+    }
+
+    void print() {print(root);}
+    int64_t hi() {return hi(root);}
+};
+
+int main() {
+    Treap t;
+    for (uint64_t i = 0; i < (1 << 25); ++i) {
+        t.insert(rd());
+    }
+    std::cout << t.hi();
+}
