cd731d249c4d5cd9ce545a11008ae421cd1b665d
Moon <mooninfinityalways@gmail.com>
2026-06-17 01:35:05 +0300

    C

--- HW/HW4/B.cpp
+++ HW/HW4/B.cpp
@@ -7,11 +7,11 @@ struct Node {
     uint64_t begin = 0, end = 1;
     Node* left = nullptr;
     Node* right = nullptr;
-    Node(uint64_t begin, uint64_t end):begin(begin), end(end) {
+    Node(uint64_t begin, uint64_t end, uint64_t value):begin(begin), end(end), value(value) {
         if (begin + 1 < end) {
             uint64_t mid = (begin + end) / 2;
-            left = new Node(begin, mid);
-            right = new Node(mid, end);
+            left = new Node(begin, mid, value);
+            right = new Node(mid, end, value);
         }
     }
     ~Node() {
@@ -22,34 +22,34 @@ struct Node {
 
 class SegmentTree {
     Node* root = nullptr;
-    void set(Node*& node, uint64_t index, int64_t value) {
+    int64_t get(Node*& node, uint64_t value) {
         if (node->begin + 1 == node->end) {
-            node->value = value;
-            return;
+            node->value -= value;
+            return node->begin;
         }
-        if (node->left->end > index) set(node->left, index, value);
-        else set(node->right, index, value);
+        uint64_t ans;
+        if (node->left->value >= value) ans = get(node->left, value);
+        else ans = get(node->right, value);
         node->value = std::max(node->left->value, node->right->value);
+        return ans;
     }
-
-    int64_t get(Node*& node, uint64_t q_begin, uint64_t q_end) {
-        if (node->begin >= q_begin && node->end <= q_end) return node->value;
-        if (node->begin >= q_end || node->end <= q_begin) return 0;
-        return std::max(get(node->left, q_begin, q_end), get(node->right, q_begin, q_end));
+    public:
+    SegmentTree(uint64_t begin, uint64_t end, uint64_t value) {root = new Node(begin, end, value);}
+    int64_t get(uint64_t value) {
+        if (root->value < value) return -1; 
+        int64_t ans = get(root, value);
+        return ans;
     }
-public:
-    SegmentTree(uint64_t begin, uint64_t end) {root = new Node(begin, end);}
-    void set(uint64_t index, int64_t value) {set(root, index, value);}
-    int64_t get(uint64_t begin, uint64_t end) {return get(root, begin, end);}
 };
 
 int main() {
     uint64_t H, W, N;
     std::cin >> H >> W >> N;
-    SegmentTree tree(0, W);
+    SegmentTree tree(1, std::min(H, N) + 1, W);
+    N++;
     while (--N) {
         uint64_t x;
         std::cin >> x;
-
+        std::cout << tree.get(x) << "\n";
     }
 }
